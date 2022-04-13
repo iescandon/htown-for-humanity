df05c5267e739946afc7a2b3e37494c3c6a96bde
Inez Escandon <iescan4@gmail.com>
2022-04-12 20:29:28 -0500

    added logo

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,10 @@ export const Home = ({content}) => {
       <section className="relative w-screen h-full min-h-min">
         {/* blue image section */}
         <div className="relative h-[550px]">
-          <div className="blueOverlay absolute w-full h-full flex justify-start items-end">
+          <div className="blueOverlay absolute w-full h-full flex flex-col justify-between">
+            <div>      
+              <img src={content[0].fields.logo.fields.file.url} className=" m-12 h-[120px]"/>
+            </div>
             <div className="title font-saira font-bold text-white text-[4em] mx-24 my-10 leading-tight">{content[0].fields.headerTitle}<br></br>{content[0].fields.headerSubtitle}</div>
           </div>
           <div className="w-full h-full">
@@ -17,9 +20,15 @@ export const Home = ({content}) => {
         </div>
         {/* yellow section */}
         <div className="w-screen min-h-[350px] py-12 pl-24 pr-[550px] bg-flagYellow">
+
+        {content[0].fields.paragraphText.content[0].content[0].value}
+        {/* {content[0].fields.paragraphText.content.map((paragraph)=>{
           <p>
-            {content[0].fields.paragraphText.content[0].content[0].value}
+            {paragraph.content[0].value}
           </p>
+          console.log(paragraph.content[0].value)
+        })
+        } */}
         </div>
         <div className="absolute w-full h-full flex top-0 justify-end items-center">
           <div className="relative bg-white w-[375px] h-[750px] mr-20"></div>
