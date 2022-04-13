786cfdc1f22ba29fecad8ffc3bc11b57488d887d
Inez Escandon <iescan4@gmail.com>
2022-04-12 20:03:29 -0500

    added contentful content

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { createClient } from "contentful";
 
 export const Home = ({content}) => {
-  // console.log(content);
+  console.log(content);
   return (
     <>
       {/* flag section */}
@@ -9,14 +9,17 @@ export const Home = ({content}) => {
         {/* blue image section */}
         <div className="relative h-[550px]">
           <div className="blueOverlay absolute w-full h-full flex justify-start items-end">
-            <div className="title font-saira font-bold text-white text-[4em] mx-24 my-10 leading-tight">H-town can help: <br></br> Donate Now</div>
+            <div className="title font-saira font-bold text-white text-[4em] mx-24 my-10 leading-tight">{content[0].fields.headerTitle}<br></br>{content[0].fields.headerSubtitle}</div>
           </div>
           <div className="w-full h-full">
             <img src={content[0].fields.heroImage.fields.file.url} alt="hero image" className="w-full h-full object-cover"></img>
           </div>
         </div>
         {/* yellow section */}
-        <div className="w-screen h-[350px] bg-flagYellow">
+        <div className="w-screen min-h-[350px] py-12 pl-24 pr-[550px] bg-flagYellow">
+          <p>
+            {content[0].fields.paragraphText.content[0].content[0].value}
+          </p>
         </div>
         <div className="absolute w-full h-full flex top-0 justify-end items-center">
           <div className="relative bg-white w-[375px] h-[750px] mr-20"></div>
@@ -31,7 +34,7 @@ export const Home = ({content}) => {
         <div className="bg-flagBlue h-full w-2/3 flex flex-row justify-center items-center">
           <p className="card bg-white m-8 w-full h-90percent"></p>
         </div>
-        <img className="bg-white h-full w-1/3 object-cover" src="https://www.politico.com/dims4/default/0081a6d/2147483647/strip/true/crop/8015x5346+0+0/resize/1260x840!/quality/90/?url=https%3A%2F%2Fstatic.politico.com%2Ff7%2F13%2F25c33f46427190c964a69f15dcae%2Fgettyimages-1366284970.jpg"/>
+        <img className="bg-white h-full w-1/3 object-cover" src={content[0].fields.contactAsideImage.fields.file.url}/>
       </section>  
       {/* 2nd white section */}
       {/* <section className="h-[500px]">
@@ -51,7 +54,7 @@ export async function getStaticProps()
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   });
-  const res = await client.getEntries({ content_type: 'images'})
+  const res = await client.getEntries({ content_type: 'pageContent'})
   return {
     props: {
       content: res.items
