2e8e1d972d8209b8105bd2b8d2c472675a397a22
Inez Escandon <iescan4@gmail.com>
2022-04-22 18:51:37 -0500

    added justify between on question and chevron

--- pages/help.js
+++ pages/help.js
@@ -96,7 +96,7 @@ export const Help = ({ content }) => {
                 <AccordionItem className={`rounded border-t border-r border-l ${i + 1 === FAQs.length ? 'border-b' : ''}`} key={`faq-${i + 1}`}>
                   <h2>
                     <AccordionButton className="font-roboto md:hover:bg-[#f3f6fd]">
-                    <div className="flex flex-row md:h-[40px] min-h-min p-3 md:p-6 w-full justify items-center">
+                    <div className="flex flex-row md:h-[40px] min-h-min p-3 md:p-6 w-full justify-between items-center">
                       <div className="text-left">
                         {item.question}
                       </div>
