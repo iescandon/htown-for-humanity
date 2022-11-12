f2cb0d2d87dfdf5eb884fa674d23d459f01443c4
iescandon <iescan4@gmail.com>
2022-11-12 17:53:10 -0600

    removed webpack config but updated all dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  future: { webpack5: true }
+  // future: { webpack5: true }
   // env:{
   //   INSTAGRAM_API_ACCESS_TOKEN: process.env.INSTAGRAM_API_ACCESS_TOKEN,
   // }
